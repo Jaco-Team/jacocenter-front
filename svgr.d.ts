f69e7cdd1ce3c287f367ead95d76271fd94bec7b
zang3tsu88 <maximenya.a@gmail.com>
2026-02-22 22:38:56 +0300

    chore(svgr): update config to work with suffix ?react

--- svgr.d.ts
+++ svgr.d.ts
@@ -1,10 +1,10 @@
-declare module '*.svg' {
-  import { FC, SVGProps } from 'react'
-  const content: FC<SVGProps<SVGElement>>
-  export default content
+declare module '*.svg?react' {
+  import { FC, SVGProps } from 'react';
+  const content: FC<SVGProps<SVGElement>>;
+  export default content;
 }
 
-declare module '*.svg?url' {
-  const content: any
-  export default content
+declare module '*.svg' {
+  const content: any;
+  export default content;
 }
