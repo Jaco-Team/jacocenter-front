797f9755f13f683522020c0be353bbd938069d35
Vladislav Ukhanov <anarho_com@gmail.com>
2026-02-25 12:55:07 +0400

    fix storibook imports

--- src/shared/ui/Button/Button.stories.ts
+++ src/shared/ui/Button/Button.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react-vite";
+import { Meta, StoryObj } from "@storybook/nextjs";
 import { Button } from "./Button";
 
 const meta: Meta<typeof Button> = {
