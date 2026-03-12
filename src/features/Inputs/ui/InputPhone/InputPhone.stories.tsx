05ff00ae69d9becc68c0ed18e7b8182d805fa185
Maksim <aldair94@mail.ru>
2026-03-13 00:06:00 +0200

    feat:create feature - InputPhone

---
+++ src/features/Inputs/ui/InputPhone/InputPhone.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import { InputPhone } from './InputPhone';
+import { useState } from 'react';
+
+const meta = {
+  title: 'features/InputPhone',
+  component: InputPhone,
+  parameters: { layout: 'centered' },
+  tags: ['autodocs'],
+} satisfies Meta<typeof InputPhone>;
+
+export default meta;
+
+type Story = StoryObj<typeof InputPhone>;
+
+export const Default: Story = {
+  args: {
+    placeholder: '999 999-99-99',
+  },
+};
+
+export const WithValue: Story = {
+  args: {
+    value: '99999999',
+  },
+};
+
+export const WithSearchIcon: Story = {
+  args: {
+    withSearchIcon: true,
+    searchIconSrc: '/icons/search.svg',
+  },
+};
---
+++ src/features/Inputs/ui/InputPhone/InputPhone.tsx
@@ -0,0 +1,102 @@
+import React, { forwardRef, useEffect, useState } from 'react';
+import { Input } from '@/shared/ui/Input/Input';
+import { IInputPhoneUIProps } from './InputPhone.types';
+import './InputPhone.styles.css';
+import Image from 'next/image';
+
+const formatPhone = (digits: string) => {
+  const part1 = digits.slice(0, 3);
+  const part2 = digits.slice(3, 6);
+  const part3 = digits.slice(6, 8);
+  const part4 = digits.slice(8, 10);
+
+  let formatted = '';
+
+  if (part1) formatted += part1;
+  if (part2) formatted += ` ${part2}`;
+  if (part3) formatted += `-${part3}`;
+  if (part4) formatted += `-${part4}`;
+
+  return formatted;
+};
+
+export const InputPhone = forwardRef<HTMLInputElement, IInputPhoneUIProps>(
+  (
+    {
+      onChange,
+      value,
+      withSearchIcon = false,
+      searchIconSrc,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    const [digits, setDigits] = useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const onlyDigits = e.target.value.replace(/\D/g, '').slice(0, 10);
+
+      setDigits(onlyDigits);
+      onChange?.(`7${onlyDigits}`);
+    };
+
+    const clearInput = () => {
+      setDigits('');
+      onChange?.('7');
+    }
+
+    const formatted = formatPhone(digits);
+    const clear = digits.length > 0;
+
+    useEffect(() => {
+      if (value !== undefined) {
+        setDigits(value);
+      }
+    }, [value]);
+
+    return (
+      <div className='phone-root'>
+        {withSearchIcon && (
+          <div className='phone-icon'>
+            <Image
+              src='/icons/search.svg'
+              alt='Поиск'
+              width={16}
+              height={16} />
+          </div>
+        )}
+
+        <span className={`phone-prefix ${withSearchIcon ? 'with-icon' : ''}`}>
+          +7
+        </span>
+
+        <Input
+          ref={ref}
+          type='tel'
+          value={formatted}
+          onChange={handleChange}
+          className={`phone-input ${withSearchIcon ? 'with-icon' : ''} ${className}`}
+          placeholder='999 999-99-99'
+          {...props}
+        />
+
+        {clear && (
+          <button
+            type='button'
+            className='phone-clear'
+            onClick={clearInput}
+            onMouseDown={(e) => e.preventDefault()}
+            aria-label='Очистить'
+          >
+            <Image
+              src='/icons/button-close.svg'
+              alt='Очистить'
+              width={16}
+              height={16} />
+          </button>
+        )}
+      </div>
+    );
+  }
+);
