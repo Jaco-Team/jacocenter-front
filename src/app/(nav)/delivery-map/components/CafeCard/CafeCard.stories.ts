3f953e8df38f2b3cf3b007da4f1a43cbd975bea2
Oksana Ivanova <nyamnyamaq@yandex.ru>
2026-05-30 22:50:12 +0300

    feat: add cafe card component

---
+++ src/app/(nav)/delivery-map/components/CafeCard/CafeCard.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { CafeCard } from "./CafeCard";
+
+const meta = {
+  title: "Features/CafeCard",
+  component: CafeCard,
+  args: {
+    name: "Ворошилова 12А",
+    zoneNumber: "1",
+    deliveryPrice: "199",
+    isSelected: false,
+    onClick: () => {},
+  },
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof CafeCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ src/app/(nav)/delivery-map/components/CafeCard/CafeCard.types.ts
@@ -0,0 +1,7 @@
+export type CafeCardProps = {
+  name: string;
+  zoneNumber: string;
+  deliveryPrice: string;
+  isSelected: boolean;
+  onClick: () => void;
+};
